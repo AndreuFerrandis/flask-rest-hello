3db409750725b3e88b076d242b5e2508444cf8ee
AndreuFerrandis <153548161+AndreuFerrandis@users.noreply.github.com>
2024-04-22 19:31:55 +0000

    app y models hay que probarlo

--- src/app.py
+++ src/app.py
@@ -17,27 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Cambiar la UR
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-# Define los modelos Planet, People y Favorite
-class Planet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    # Agrega más campos según sea necesario
-
-class People(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    # Agrega más campos según sea necesario
-
-class Favorite(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
-    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
-
-    # Define las relaciones entre Favorite y User, Planet, People
-    user = db.relationship('User', backref=db.backref('favorites', lazy=True))
-    planet = db.relationship('Planet', backref=db.backref('favorites', lazy=True))
-    people = db.relationship('People', backref=db.backref('favorites', lazy=True))
 
 # Implementa los endpoints GET
 @app.route('/users', methods=['GET'])
--- src/models.py
+++ src/models.py
@@ -2,18 +2,23 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    # Agrega más campos según sea necesario
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    # Agrega más campos según sea necesario
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'), nullable=True)
+
+    # Define las relaciones entre Favorite y User, Planet, People
+    user = db.relationship('User', backref=db.backref('favorites', lazy=True))
+    planet = db.relationship('Planet', backref=db.backref('favorites_planets', lazy=True))
+    people = db.relationship('People', backref=db.backref('favorites_people', lazy=True))
